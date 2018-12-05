9236773a324d149a16fe70c3233541fafda56835
mpocta30 <mpocta30@vt.edu>
2018-12-05 15:32:44 -0500

    Correctly changed code to obtain individual braille characters

--- OCR/create_templates.m
+++ OCR/create_templates.m
@@ -1,18 +1,18 @@
 %CREATE TEMPLATES
 %Letter
-A=imresize(imread('dataset_1\a.jpg'), [62 42]);B=imresize(imread('dataset_1\b.jpg'), [62 42]);
-C=imresize(imread('dataset_1\c.jpg'), [62 42]);D=imresize(imread('dataset_1\d.jpg'), [62 42]);
-E=imresize(imread('dataset_1\e.jpg'), [62 42]);F=imresize(imread('dataset_1\f.jpg'), [62 42]);
-G=imresize(imread('dataset_1\g.jpg'), [62 42]);H=imresize(imread('dataset_1\h.jpg'), [62 42]);
-I=imresize(imread('dataset_1\i.jpg'), [62 42]);J=imresize(imread('dataset_1\j.jpg'), [62 42]);
-K=imresize(imread('dataset_1\k.jpg'), [62 42]);L=imresize(imread('dataset_1\l.jpg'), [62 42]);
-M=imresize(imread('dataset_1\m.jpg'), [62 42]);N=imresize(imread('dataset_1\n.jpg'), [62 42]);
-O=imresize(imread('dataset_1\o.jpg'), [62 42]);P=imresize(imread('dataset_1\p.jpg'), [62 42]);
-Q=imresize(imread('dataset_1\q.jpg'), [62 42]);R=imresize(imread('dataset_1\r.jpg'), [62 42]);
-S=imresize(imread('dataset_1\s.jpg'), [62 42]);T=imresize(imread('dataset_1\t.jpg'), [62 42]);
-U=imresize(imread('dataset_1\u.jpg'), [62 42]);V=imresize(imread('dataset_1\v.jpg'), [62 42]);
-W=imresize(imread('dataset_1\w.jpg'), [62 42]);X=imresize(imread('dataset_1\x.jpg'), [62 42]);
-Y=imresize(imread('dataset_1\y.jpg'), [62 42]);Z=imresize(imread('dataset_1\z.jpg'), [62 42]);
+A=imresize(imread('dataset_1\a.jpg'), [42 24]);B=imresize(imread('dataset_1\b.jpg'), [42 24]);
+C=imresize(imread('dataset_1\c.jpg'), [42 24]);D=imresize(imread('dataset_1\d.jpg'), [42 24]);
+E=imresize(imread('dataset_1\e.jpg'), [42 24]);F=imresize(imread('dataset_1\f.jpg'), [42 24]);
+G=imresize(imread('dataset_1\g.jpg'), [42 24]);H=imresize(imread('dataset_1\h.jpg'), [42 24]);
+I=imresize(imread('dataset_1\i.jpg'), [42 24]);J=imresize(imread('dataset_1\j.jpg'), [42 24]);
+K=imresize(imread('dataset_1\k.jpg'), [42 24]);L=imresize(imread('dataset_1\l.jpg'), [42 24]);
+M=imresize(imread('dataset_1\m.jpg'), [42 24]);N=imresize(imread('dataset_1\n.jpg'), [42 24]);
+O=imresize(imread('dataset_1\o.jpg'), [42 24]);P=imresize(imread('dataset_1\p.jpg'), [42 24]);
+Q=imresize(imread('dataset_1\q.jpg'), [42 24]);R=imresize(imread('dataset_1\r.jpg'), [42 24]);
+S=imresize(imread('dataset_1\s.jpg'), [42 24]);T=imresize(imread('dataset_1\t.jpg'), [42 24]);
+U=imresize(imread('dataset_1\u.jpg'), [42 24]);V=imresize(imread('dataset_1\v.jpg'), [42 24]);
+W=imresize(imread('dataset_1\w.jpg'), [42 24]);X=imresize(imread('dataset_1\x.jpg'), [42 24]);
+Y=imresize(imread('dataset_1\y.jpg'), [42 24]);Z=imresize(imread('dataset_1\z.jpg'), [42 24]);
 %Number
 % one=imread('dataset_1\1.jpg');  two=imread('dataset_1\2.jpg');
 % three=imread('dataset_1\3.jpg');four=imread('dataset_1\4.jpg');
@@ -22,6 +22,13 @@ Y=imresize(imread('dataset_1\y.jpg'), [62 42]);Z=imresize(imread('dataset_1\z.jp
 %*-*-*-*-*-*-*-*-*-*-*-
 letter=[A B C D E F G H I J K L M...
     N O P Q R S T U V W X Y Z];
+
+letter = rgb2gray(letter);
+threshold = graythresh(letter);
+letter =~imbinarize(letter,threshold);
+% Remove all object containing fewer than 30 pixels
+letter = bwareaopen(letter,30);
+
 % number=[one two three four five...
 %     six seven eight nine zero];
 % character=[letter number];
@@ -29,7 +36,6 @@ character=letter;
 templates=mat2cell(character,42,[24 24 24 24 24 24 24 ...
     24 24 24 24 24 24 24 ...
     24 24 24 24 24 24 24 ...
-    24 24 24 24 24 24 24 ...
-    24 24 24 24 24 24 24 24]);
+    24 24 24 24 24]);
 save ('templates','templates')
 clear all
--- OCR/lines.m
+++ OCR/lines.m
@@ -1,26 +1,29 @@
-function [fl re]=lines(im_texto)
+function [fl re]=lines(im_braille)
 % Divide text in lines
-% im_texto->input image; fl->first line; re->remain line
+% im_braille->input image; fl->first line; re->remain line
 % Example:
-% im_texto=imread('TEST_3.jpg');
-% [fl re]=lines(im_texto);
-% subplot(3,1,1);imshow(im_texto);title('INPUT IMAGE')
+% im_braille=imread('TEST_3.jpg');
+% [fl re]=lines(im_braille);
+% subplot(3,1,1);imshow(im_braille);title('INPUT IMAGE')
 % subplot(3,1,2);imshow(fl);title('FIRST LINE')
 % subplot(3,1,3);imshow(re);title('REMAIN LINES')
-im_texto=clip(im_texto);
-num_filas=size(im_texto,1);
-for s=1:num_filas
-    if sum(im_texto(s,:))==0
-        nm=im_texto(1:s-1, :); % First line matrix
-        rm=im_texto(s:end, :);% Remain line matrix
+space_counter = 0;
+im_braille=clip(im_braille);
+pixel_line_num=size(im_braille,1);
+for s=1:pixel_line_num
+    if sum(im_braille(s,:))==0 && space_counter >= 20
+        nm=im_braille(1:s-1, :); % First line matrix
+        rm=im_braille(s:end, :);% Remain line matrix
         fl = clip(nm);
         re=clip(rm);
         %*-*-*Uncomment lines below to see the result*-*-*-*-
         %         subplot(2,1,1);imshow(fl);
         %         subplot(2,1,2);imshow(re);
         break
+    elseif sum(im_braille(s,:))==0
+        space_counter = space_counter + 1;
     else
-        fl=im_texto;%Only one line.
+        fl=im_braille;%Only one line.
         re=[ ];
     end
 end
